82829a036934ecbd11945bee4c792badf0da2b38
kartsridhar <contactkarthik34@gmail.com>
2019-08-05 00:50:08 +0100

    added sorting sol

---
+++ sorting/comparator.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Player {
+	String name;
+	int score;
+
+	Player(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+}
+
+class Checker implements Comparator<Player> {
+  	// complete this method
+	public int compare(Player a, Player b) {
+        if (a.score > b.score) {
+            return -1;
+        }
+        else if (a.score < b.score) {
+            return 1;
+        }
+        else {
+            return a.name.compareTo(b.name);
+        }
+    }
+}
+
