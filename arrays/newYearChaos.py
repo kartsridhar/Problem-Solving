27cd9a636aa094e43bfc1dfdc51aea141f2abcab
kartsridhar <contactkarthik34@gmail.com>
2019-08-07 13:14:19 +0100

    added 4th array sol

--- arrays/newYearChaos.py
+++ arrays/newYearChaos.py
@@ -13,16 +13,16 @@ def swap(q, i, j):
 
 # Complete the minimumBribes function below.
 def minimumBribes(q):
-    bribeCount = 0
-    for i in range(len(q)):
-        if q[i] - (i + 1) > 2:
-            print("Too chaotic")
-            return
-        for j in range(i + 1, len(q)):
-            if q[i] > q[j]:
-                swap(q, j, i)
-                bribeCount += 1
-    print(bribeCount)
+    count = 0
+    for _ in range(2):
+        for i in range(len(q) - 1, 0, -1):
+            if q[i] < q[i-1]:
+                swap(q, i, i-1)
+                count += 1
+    if q != sorted(q):
+        print("Too chaotic")
+    else:
+        print(count)
 
 if __name__ == '__main__':
     t = int(input())
