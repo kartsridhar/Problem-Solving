3dd39bb4b8a90cd9519482073a3aeb258cc40543
kartsridhar <contactkarthik34@gmail.com>
2019-08-25 00:48:02 +0100

    hardest EASY problem, still failing 9 tests

---
+++ Problem-Solving/Strings/twoCharacters.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from itertools import combinations
+
+# Complete the alternate function below.
+def alternate(s):
+    alpha = list(s)
+    longest = 0
+    for i in combinations(alpha, 2):
+        separator = ""
+        substring = separator.join(j for j in s if j in i)     
+        if all(substring[k] != substring[k+1] for k in range(len(substring) - 1)):
+            longest = max(longest, len(substring))
+    return longest
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    l = int(input().strip())
+
+    s = input()
+
+    result = alternate(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
