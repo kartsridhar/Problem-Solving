d0213925084eab71c32a549168099159ac4c4356
kartsridhar <contactkarthik34@gmail.com>
2020-08-04 10:18:34 +0100

    added comments to sol

---
+++ HackerRank/Interview-Prep-Kit/greedyAlgorithms/minimumAbsoluteDiff.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    minVal = 99999
+    for i in range(len(arr)-1):
+        for j in range(i+1,len(arr)):
+            if abs(arr[i] - arr[j]) < minVal:
+                minVal = abs(arr[i] - arr[j])
+    return minVal
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
--- HackerRank/Problem-Solving/Basic-Certification/passwordDecryption.py
+++ HackerRank/Problem-Solving/Basic-Certification/passwordDecryption.py
@@ -15,55 +15,71 @@ import sys
 # The function accepts STRING s as parameter.
 #
 
+# Function to get all the numbers in the password
 def getAllNumbers(s):
     nums = []
     for i in range(len(s)):
         if s[i].isnumeric() and s[i] != '0':
             nums.append(s[i])
+    
+    # Returning the reversed list as we want to swap the 0s in order
     return nums[::-1]
 
+# Function to replace all the 0s with the respective original numbers
 def replaceAllNumbers(s):
     nums = getAllNumbers(s)
-    numIndex = 0
-    s_new = ""
+    numIndex = 0            # index to access from the nums list
+    s_new = ""              # updated encrypted password string to return
     for i in range(len(s)):
-        if s[i] == '0':
+        if s[i] == '0':     # if it is 0, append the respective number from nums at numIndex to the new string
             s_new += nums[numIndex]
             numIndex += 1
-        else:
+        else:               # otherwise, just append the respective character
             s_new += s[i]
+    
+    # Since the encrypted password contains the numbers at the start, returning the replaced numbers by trimming all the numbers at
+    # the start
     return s_new[len(nums):]
 
+# Function to get all the substrings to replace the *s with
 def getAllStars(s):
     s = replaceAllNumbers(s)
-    s = s[::-1]
-    substrings = []
+    s = s[::-1]             # reversing the string to make it easier
+    substrings = []         # list to store the substrings to replace the *s with
     for i in range(len(s)-1):
         if s[i] == '*':
-            substrings.append(s[i+1:i+3] + '$')
+            substrings.append(s[i+1:i+3] + '$')     # since the string is reversed, store the respective slice delimited with a $ symbol
+                                                    # this is to ensure that the substring to replace the encryptions are of the same length
+    
+    # Returning the substrings list reversed as we reversed the string initially
     return substrings[::-1]
 
 def decryptPassword(s):
     # Write your code here
     substrings = getAllStars(s)
-    starCount = 0
-    s = replaceAllNumbers(s)
-    s_copy = s
+    starIndex = 0               # index to access from the substrings list
+    s = replaceAllNumbers(s)    # replacing all the numbers of the string
+    s_copy = s                  # working with a copy of the string to do all the replacements
     for i in range(len(s)):
         if s[i] == '*':
-            s_copy = s_copy.replace(s_copy[i-2:i+1], substrings[starCount])
-            starCount += 1
+            # if a star is found, replace the previous 2 characters and the * with the respective substring
+            s_copy = s_copy.replace(s_copy[i-2:i+1], substrings[starIndex])
+            starIndex += 1
+    
+    # remove all the $ delimiters
     s_copy = s_copy.replace('$', '')
     return s_copy
 
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    s = input()
+    # s = input()
+    s = '43Ah*ck0rr0nk'
 
     result = decryptPassword(s)
+    # print(result)
 
-    fptr.write(result + '\n')
+    # fptr.write(result + '\n')
 
-    fptr.close()
+    # fptr.close()
